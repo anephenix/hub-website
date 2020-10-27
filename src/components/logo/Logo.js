b84be4e02eda582a572ab7d6a8aec27f3ae65b48
Paul Jensen <paulbjensen@gmail.com>
2020-10-27 20:30:20 +0000

    A bit of refactoring plus updated Next

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,15 @@
+
+import './Logo.scss';
+import Link from 'next/link';
+
+const Logo = () => (
+    <Link href="/">
+        <div
+            id="logo"
+        >
+            Hub
+        </div>
+    </Link>
+);
+			
+export default Logo;
---
+++ src/components/logo/Logo.test.js
@@ -0,0 +1,6 @@
+import Logo from './Logo';
+
+describe('Logo', () => {
+	Logo;
+	test.todo('should do something');
+});
