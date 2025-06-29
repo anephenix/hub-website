9ac065f4cae1e0fad03a1989d53162cc5f7dade8
Paul Jensen <paulbjensen@gmail.com>
2025-06-29 22:24:51 +0100

    More small tweaks

--- pages/get-started.js
+++ pages/get-started.js
@@ -2,7 +2,7 @@ const GetStartedPage = () => (
     <>
         <h1>Get started</h1>
         <p><i>Well, this is looking a bit bare...</i></p>
-        <p>Not to worry, we're going to update it. In the meantime, checkout the <a href="https://github.com/anephenix/hub/blob/main/README.md" rel="noopen noreferrer" target="_blank">README on GitHub.</a></p>
+        <p>Not to worry, we're going to update it. In the meantime, checkout the <a href="https://github.com/anephenix/hub/blob/master/README.md" rel="noopen noreferrer" target="_blank">README on GitHub.</a></p>
     </>
 );
 
